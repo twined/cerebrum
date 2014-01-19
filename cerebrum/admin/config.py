4543cb476ccf634c77a659fbe658f5db2cb16350
Trond Mjøen <trond@twined.net>
2014-01-20 00:43:59 +0100

    - testing template stuff

--- cerebrum/admin/config.py
+++ cerebrum/admin/config.py
@@ -1,16 +1,22 @@
 from django.core.urlresolvers import reverse_lazy
 
 APP_ADMIN_MENU = {
-    'Administrasjon': {
-        'Hjem': {
-            'url': reverse_lazy('admin:dashboard'),
-            'icon': 'icon-home',
-            'order': 0,
-        },
-        'Slett cache': {
-            'url': reverse_lazy('admin:cachebust'),
-            'icon': 'icon-trash',
-            'order': 0,
-        },
+    'Admin': {
+        'anchor': 'admin',
+        'bgcolor': '#FB6B5B',
+        'icon': 'fa fa-dashboard icon',
+
+        'menu': {
+            'Hjem': {
+                'url': reverse_lazy('admin:dashboard'),
+                'icon': 'icon-home',
+                'order': 0,
+            },
+            'Slett cache': {
+                'url': reverse_lazy('admin:cachebust'),
+                'icon': 'icon-trash',
+                'order': 0,
+            },
+        }
     }
 }
